123c4b8ea9a6d62237954299ec953e18c54ab872
Seine <>
2024-05-27 01:42:16 +0900

    20240526 persistence Context

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -31,16 +31,33 @@ public class JpaMain {
 //            em.remove(findMember);
 
             //update
- //           findMember.setName("HelloJPA");
+//           findMember.setName("HelloJPA");
 
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
- //                   .setFirstResult(5) //페이징
+//            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+//                   .setFirstResult(5) //페이징
 //                    .setMaxResults(8)
-                    .getResultList();
+//                    .getResultList();
 
-            for (Member m : resultList) {
-                System.out.println(m.getName());
-            }
+//            for (Member m : resultList) {
+//                System.out.println(m.getName());
+//            }
+
+            //비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA2");
+
+            //영속
+            System.out.println("===== BEFORE =====");
+            em.persist(member);
+            System.out.println("===== AFTER =====");
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+            //준영속 detach...
 
             tx.commit();
         } catch (Exception e) {
