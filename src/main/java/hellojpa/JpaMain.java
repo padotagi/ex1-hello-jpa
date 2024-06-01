64f02f9e96ed9cae0237b9afaf59ff9c725e516f
Seine <>
2024-06-01 23:30:02 +0900

    20240601 unidirectional, bidirectional associations

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -43,21 +43,54 @@ public class JpaMain {
 //            }
 
             //비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA2");
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA2");
 
             //영속
-            System.out.println("===== BEFORE =====");
-            em.persist(member);
-            System.out.println("===== AFTER =====");
+//            System.out.println("===== BEFORE =====");
+//            em.persist(member);
+//            System.out.println("===== AFTER =====");
 
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+//            Member findMember = em.find(Member.class, 101L);
+//
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
 
             //준영속 detach...
+
+            /* 단방향 연관관계 */
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+ //           team.getMembers().add(member);
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.changeTeam(team);
+            em.persist(member);
+
+
+            em.flush();
+            em.clear();
+
+//            Member findMember = em.find(Member.class, member.getId());
+
+//            Team findTeam = findMember.getTeam();
+//            System.out.println("findTeam = " + findTeam.getName());
+//
+//            // FK update
+//            Team newTeam = em.find(Team.class, 100L);
+//            findMember.setTeam(newTeam);
+
+            /* 양방향 연관관계 */
+//            List<Member> members = findMember.getTeam().getMembers();
+//            for (Member m : members) {
+//                System.out.println("m = " + m.getUsername());
+//            }
+
+
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,26 +1,26 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-//@Table(name = "USER")
+//@Table(name = "MBR")
 public class Member {
 
     @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    //@Column(name = "USERNAME")
-    private String name;
 
-    public Member() {
-    }
+    @Column(name = "USERNAME")
+    private String username;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
 
     public Long getId() {
         return id;
@@ -30,11 +30,21 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+
+        team.getMembers().add(this); // 연관관계 편의 메소드 (권장)
     }
 }
